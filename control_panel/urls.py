c89980f8bf916f80fad47b0323cd168740b7fc95
Bruno Lima <bruno.vn.lima@gmail.com>
2026-06-18 21:35:05 -0300

    feat: Add device detail flow

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from .views import DevicesList, UpdateDevice, CreateDevice, UpdateMenuItem, DeleteMenuItem, DeliveredOrders, PendingOrders, OrderDetail, CreateMenuItem
+from .views import DevicesList, UpdateDevice, CreateDevice, UpdateMenuItem, DeleteMenuItem, DeliveredOrders, PendingOrders, DeviceDetail, OrderDetail, CreateMenuItem
 
 urlpatterns = [
     path("devices/", DevicesList.as_view(), name="devices"), # Lista todos os dispositivos
 
     path("device/", include([
-        path("<int:pk>/update/", UpdateDevice.as_view(), name="update"), # Atualiza dados de um dispositivo específico,
-        path("create/", CreateDevice.as_view(), name="create") # Cria um novo dispositivo via admin
+        path("create/", CreateDevice.as_view(), name="create"), # Cria um novo dispositivo via admin
+
+        path("<int:pk>/", include([
+            path("update/", UpdateDevice.as_view(), name="update"), # Atualiza dados de um dispositivo específico
+            path("detail/", DeviceDetail.as_view(), name="detail") # Mostra os detalhes de um dispositivo específico
+        ])),
+
     ])),
 
     path("item/", include([
