e6e8434635420a59f24e8fb988d91c93dc0dfe79
Bruno Lima <bruno.vn.lima@gmail.com>
2026-06-29 19:25:14 -0300

    conflit fix

--- control_panel/views.py
+++ control_panel/views.py
@@ -10,6 +10,7 @@ from restaurant_menu.services import get_menu_item_by_id
 from kitchen.models import Order
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import DeliveredOrdersFilter, PendingOrdersFilter
+
 class DevicesList(ListAPIView): # View responsável por listar todos os dispositivos
     permission_classes = [permissions.IsAdminUser]
     serializer_class = DeviceListSerializer
@@ -30,6 +31,7 @@ class UpdateDevice(APIView): # View responsável por atualizar dados de um dispo
         serializer.save()
 
         return Response(status=status.HTTP_200_OK)
+    
 class CreateDevice(APIView): # View responsável por criar um dispositivo via admin
     permission_classes = [permissions.IsAdminUser]
 
@@ -55,6 +57,7 @@ class UpdateMenuItem(APIView): # View responsável por atualizar dados de um ite
         serializer.save()
 
         return Response(status=status.HTTP_200_OK)
+    
 class DeleteMenuItem(APIView): # View responsável por deletar um item do menu
     permission_classes = [permissions.IsAdminUser]
 
@@ -71,6 +74,7 @@ class DeleteMenuItem(APIView): # View responsável por deletar um item do menu
         menu_item.delete()
         
         return Response(status.HTTP_204_NO_CONTENT)
+    
 class DeliveredOrders(ListAPIView): # View responsável por listar os pedidos entregues
     permission_classes = [permissions.IsAdminUser]
     queryset = Order.objects.filter(delivered=True)
@@ -79,6 +83,7 @@ class DeliveredOrders(ListAPIView): # View responsável por listar os pedidos en
     
     filter_backends = [DjangoFilterBackend]
     filterset_class = DeliveredOrdersFilter # Classe de filtro por data
+
 class PendingOrders(ListAPIView): # View responsável por listar os pedidos não entregues
     permission_classes = [permissions.IsAdminUser]
     queryset = Order.objects.filter(delivered=False)
